f22bacabb919cca371447977b309f845691f00f2
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-17 10:08:44 +0000

    Created test for multiplication

---
+++ tests/multiplicationTest.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<gtest/gtest.h>
+#include "calculator.hpp"
+
+TEST(multiplication,multipliying_Positive_numbers)
+{
+    EXPECT_EQ(multiplication(5,6),30); 
+}
+
+TEST(multiplication,multipliying_Negative_numbers)
+{
+    EXPECT_EQ(multiplication(5,-6),-30); 
+}
+
+TEST(multiplication,multipliying_zero_to_a_Number)
+{
+    EXPECT_EQ(multiplication(5,0),0);   
+}
+
+
+
+TEST(multiplication,computing_with_Extra_Large_Number)
+{
+    EXPECT_EQ(multiplication(100000,100000),10000000000);   
+}
+
+
+
+
+int main(int argc,char** argv)
+{
+    testing:: InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
