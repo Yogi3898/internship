c1e08d35164c077ece485563f032907a9e4cd578
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-17 06:10:13 +0000

    Updated the variables name

--- sampleCode/test.cpp
+++ sampleCode/test.cpp
@@ -3,28 +3,28 @@
 #define MAX 1000
 using namespace std;
 
-int ans;
+int result;
 int TOP;
 int STACK[MAX];
 
-int add(int a,int b){
-    ans=a+b;
-    return ans;
+int addition(int a,int b){
+    result=a+b;
+    return result;
 }
 
-int sub(int a,int b){
-    ans=a-b;
-    return ans;
+int subtraction(int a,int b){
+    result=a-b;
+    return result;
 }
 
-int mul(int a,int b){
-    ans=a*b;
-    return ans;
+int multiplication(int a,int b){
+    result=a*b;
+    return result;
 }
 
-int divide(int a,int b){
-    ans=a/b;
-    return ans;
+int division(int a,int b){
+    result=a/b;
+    return result;
 }
  
 void initStack(){
@@ -52,7 +52,7 @@ void push(int num){
     }
     ++TOP;
     STACK[TOP]=num;
-}a
+}
  
 void display(){
     int i;
@@ -67,24 +67,24 @@ void display(){
     cout<<endl;
 }
  
-/*int main()
+int main()
 {
     initStack();
-    int firstNumber, secondNumber, answer, output;
+    int firstNumber, secondNumber, resultwer, output;
     char input;
     cout << "Instructions:\n\nPress + to add\nPress - to subtract\nPress * to multiply\nPress / to divide\nPress c to clear\nPress m to save\n " ;
     cin >> firstNumber >> input >>secondNumber;
     if(input =='+'){
-        output=add(firstNumber,secondNumber);
+        output=addition(firstNumber,secondNumber);
     }
     else if(input == '-'){
-        output=sub(firstNumber,secondNumber);
+        output=subtraction(firstNumber,secondNumber);
     }
     else if(input == '*'){
-        output=mul(firstNumber,secondNumber);
+        output=multiplication(firstNumber,secondNumber);
     }
    else if(input == '/'){
-        output=divide(firstNumber,secondNumber);
+        output=division(firstNumber,secondNumber);
     }
     else if(input == 'c'){
         exit(0);
@@ -93,161 +93,35 @@ void display(){
     cout << output;
     
     for(;;){
-        char input1;
+        char operat;
         int number;
-        cin >> input1;
-        if(input1 == 'c'){
+        cin >> operat;
+        if(operat == 'c'){
             display();
             exit(0);
         }
-        else if(input1 == '+'){
+        else if(operat == '+'){
             cin >> number;
-            output=add(output,number);
+            output=addition(output,number);
         }
         
-        else if(input1 == '-'){
+        else if(operat == '-'){
             cin >> number;
-            output=sub(output,number);
+            output=subtraction(output,number);
         }
-        else if(input1 == '*'){
+        else if(operat == '*'){
             cin >> number;
-            output=mul(output,number);
+            output=multiplication(output,number);
         }
-        else if(input1 == '/'){
+        else if(operat == '/'){
             cin >> number;
-            output=divide(output,number);
+            output=division(output,number);
         }
-        else if(input1 == 'm'){
+        else if(operat == 'm'){
             push(output);
         }
        cout<<output;
     }
     
     return 0;
-}*/#include <iostream>
-#include <stdlib.h>
-#define MAX 1000
-using namespace std;
-
-int ans;
-int TOP;
-int STACK[MAX];
-
-int add(int a,int b){
-    ans=a+b;
-    return ans;
 }
-
-int sub(int a,int b){
-    ans=a-b;
-    return ans;
-}
-
-int mul(int a,int b){
-    ans=a*b;
-    return ans;
-}
-
-int divide(int a,int b){
-    ans=a/b;
-    return ans;
-}
- 
-void initStack(){
-    TOP=-1;
-}
-
-int isEmpty(){
-    if(TOP==-1)
-        return 1;
-    else
-        return 0;
-}
- 
-int isFull(){
-    if(TOP==MAX-1)
-        return 1;
-    else   
-        return 0;
-}
- 
-void push(int num){
-    if(isFull()){
-        cout<<"STACK is FULL."<<endl;
-        return; 
-    }
-    ++TOP;
-    STACK[TOP]=num;
-}a
- 
-void display(){
-    int i;
-    cout<<"Your memory is:";
-    if(isEmpty()){
-        cout<<"Nothing in memory"<<endl;
-        return;
-    }
-    for(i=TOP;i>=0;i--){
-        cout<<STACK[i]<<" ";
-    }
-    cout<<endl;
-}
- 
-/*int main()
-{
-    initStack();
-    int firstNumber, secondNumber, answer, output;
-    char input;
-    cout << "Instructions:\n\nPress + to add\nPress - to subtract\nPress * to multiply\nPress / to divide\nPress c to clear\nPress m to save\n " ;
-    cin >> firstNumber >> input >>secondNumber;
-    if(input =='+'){
-        output=add(firstNumber,secondNumber);
-    }
-    else if(input == '-'){
-        output=sub(firstNumber,secondNumber);
-    }
-    else if(input == '*'){
-        output=mul(firstNumber,secondNumber);
-    }
-   else if(input == '/'){
-        output=divide(firstNumber,secondNumber);
-    }
-    else if(input == 'c'){
-        exit(0);
-    }
-    
-    cout << output;
-    
-    for(;;){
-        char input1;
-        int number;
-        cin >> input1;
-        if(input1 == 'c'){
-            display();
-            exit(0);
-        }
-        else if(input1 == '+'){
-            cin >> number;
-            output=add(output,number);
-        }
-        
-        else if(input1 == '-'){
-            cin >> number;
-            output=sub(output,number);
-        }
-        else if(input1 == '*'){
-            cin >> number;
-            output=mul(output,number);
-        }
-        else if(input1 == '/'){
-            cin >> number;
-            output=divide(output,number);
-        }
-        else if(input1 == 'm'){
-            push(output);
-        }
-       cout<<output;
-    }
-    
-    return 0;
-}*/
