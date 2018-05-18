2c4027fb001c9bbe42c986f24f0fdfe519ac51f7
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 06:48:13 +0000

    updated Subtraction Test

--- tests/subtractionTest.cpp
+++ tests/subtractionTest.cpp
@@ -4,24 +4,24 @@
 
 TEST(subtraction,subtracting_Positive_numbers)
 {
-    EXPECT_EQ(subtraction(5,6),-1); 
+    EXPECT_EQ(func.subtraction(5,6),-1); 
 }
 
 TEST(subtraction,subtracting_Negative_numbers)
 {
-    EXPECT_EQ(subtraction(5,-6),11); 
+    EXPECT_EQ(func.subtraction(5,-6),11); 
 }
 
 TEST(subtraction,subtracting_zero_to_a_Number)
 {
-    EXPECT_EQ(subtraction(5,0),5);   
+    EXPECT_EQ(func.subtraction(5,0),5);   
 }
 
 
 
 TEST(subtraction,computing_with_Extra_Large_Number)
 {
-    EXPECT_EQ(subtraction(2500000000,-2700000000),5200000000);   
+    EXPECT_EQ(func.subtraction(2500000000,-2700000000),5200000000);   
 }
 
 int main(int argc,char** argv)
