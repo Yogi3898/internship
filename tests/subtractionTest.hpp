bf5126eb018eb6eab983f010258695acb016b6fc
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 07:43:14 +0000

    converted subtraction test file to a header file

---
+++ tests/subtractionTest.hpp
@@ -0,0 +1,23 @@
+#include<gtest/gtest.h>
+
+TEST(subtraction,subtracting_Positive_numbers)
+{
+    EXPECT_EQ(func.subtraction(5,6),-1); 
+}
+
+TEST(subtraction,subtracting_Negative_numbers)
+{
+    EXPECT_EQ(func.subtraction(5,-6),11); 
+}
+
+TEST(subtraction,subtracting_zero_to_a_Number)
+{
+    EXPECT_EQ(func.subtraction(5,0),5);   
+}
+
+
+
+TEST(subtraction,computing_with_Extra_Large_Number)
+{
+    EXPECT_EQ(func.subtraction(2500000000,-2700000000),5200000000);   
+}
