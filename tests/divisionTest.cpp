46a7d8c1593d485bb2c9619a37b41774eed419e0
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 06:49:36 +0000

    updated division  Test

---
+++ tests/divisionTest.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include<gtest/gtest.h>
+#include "calculator.hpp"
+
+//functions func;
+
+TEST(division,dividing_Positive_numbers)
+{
+    EXPECT_EQ(func.division(10,2),5); 
+}
+
+TEST(division,dividing_Negative_numbers)
+{
+    EXPECT_EQ(func.division(10,-2),-5); 
+}
+
+TEST(division,When_Denominator_is_Zero)
+{
+    EXPECT_EQ(func.division(5,0),-1);   
+}
+
+TEST(division,When_Neumerator_is_Zero)
+{
+    EXPECT_EQ(func.division(0,5),0);   
+}
+
+
+
+TEST(division,computing_with_Extra_Large_Number)
+{
+    EXPECT_EQ(func.division(100000000,0.001),100000000000);   
+}
+
+
+
+
+int main(int argc,char** argv)
+{
+    testing:: InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
