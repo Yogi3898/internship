c1281828f49ee92cf628c25600a887c87255381f
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 06:45:49 +0000

    Created test for choose Function

---
+++ tests/chooseTest.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include<gtest/gtest.h>
+#include<gmock/gmock.h>
+#include "calculator.hpp"
+
+using namespace std;
+using ::testing :: AtLeast;
+using ::testing :: Return;
+using ::testing ::_;
+
+class Mockfunctions : public functions{
+public:
+    MOCK_METHOD2(addition, int(int firstNumber,int secondNumber));
+    MOCK_METHOD2(subtraction, int(int firstNumber,int secondNumber));
+    MOCK_METHOD2(multiplication, int(int firstNumber,int secondNumber));
+    MOCK_METHOD2(division, int(int firstNumber,int secondNumber));
+};
+
+
+TEST(chooseTest,addition)
+{
+    Mockfunctions mf;
+    /*
+    EXPECT_CALL(mf, addition(8,3))
+    .Times(1)
+    .WillOnce(Return(c));
+    */
+    ON_CALL(mf,addition(2,3)).WillByDefault(Return(2+3));
+    
+    int retvalue=choose(2,'+',3);
+    EXPECT_EQ(retvalue,5);
+    
+}
+
+TEST(chooseTest,subtraction)
+{
+    Mockfunctions mf;
+    ON_CALL(mf,subtraction(2,3)).WillByDefault(Return(2-3));
+    
+    int retvalue=choose(2,'-',3);
+    EXPECT_EQ(retvalue,-1);
+}
+
+TEST(chooseTest,multiplication)
+{
+    Mockfunctions mf;
+    ON_CALL(mf,multiplication(2,3)).WillByDefault(Return(2*3));
+    
+    int retvalue=choose(2,'*',3);
+    EXPECT_EQ(retvalue,6);
+}
+
+TEST(chooseTest,division)
+{
+    Mockfunctions mf;
+    ON_CALL(mf,division(6,3)).WillByDefault(Return(6/3));
+    
+    int retvalue=choose(6,'/',3);
+    EXPECT_EQ(retvalue,2);
+}
+
+
+
+
+int main(int argc,char** argv)
+{
+    testing:: InitGoogleTest(&argc,argv);
+    return RUN_ALL_TESTS();
+}
