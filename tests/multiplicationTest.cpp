dcaf179df16ecc063fa12e004c1b5f60beed2e79
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 06:48:57 +0000

    updated Multiplication Test

--- tests/multiplicationTest.cpp
+++ tests/multiplicationTest.cpp
@@ -4,24 +4,24 @@
 
 TEST(multiplication,multipliying_Positive_numbers)
 {
-    EXPECT_EQ(multiplication(5,6),30); 
+    EXPECT_EQ(func.multiplication(5,6),30); 
 }
 
 TEST(multiplication,multipliying_Negative_numbers)
 {
-    EXPECT_EQ(multiplication(5,-6),-30); 
+    EXPECT_EQ(func.multiplication(5,-6),-30); 
 }
 
 TEST(multiplication,multipliying_zero_to_a_Number)
 {
-    EXPECT_EQ(multiplication(5,0),0);   
+    EXPECT_EQ(func.multiplication(5,0),0);   
 }
 
 
 
 TEST(multiplication,computing_with_Extra_Large_Number)
 {
-    EXPECT_EQ(multiplication(100000,100000),10000000000);   
+    EXPECT_EQ(func.multiplication(100000,100000),10000000000);   
 }
 
 
