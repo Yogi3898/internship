ab2258c46bd454306f0b663fe6fe823ae26357eb
Yogesh Modi <16uec134@lnmiit.ac.in>
2018-05-18 06:47:18 +0000

    updated AdditionTest

--- tests/additionTest.cpp
+++ tests/additionTest.cpp
@@ -4,24 +4,24 @@
 
 TEST(Addition,adding_Positive_numbers)
 {
-    EXPECT_EQ(addition(5,6),11); 
+    EXPECT_EQ(func.addition(5,6),11); 
 }
 
 TEST(Addition,adding_Negative_numbers)
 {
-    EXPECT_EQ(addition(5,-6),-1); 
+    EXPECT_EQ(func.addition(5,-6),-1); 
 }
 
 TEST(Addition,adding_zero_to_a_Number)
 {
-    EXPECT_EQ(addition(5,0),5);   
+    EXPECT_EQ(func.addition(5,0),5);   
 }
 
 
 
 TEST(Addition,computing_with_Extra_Large_Number)
 {
-    EXPECT_EQ(addition(2500000000,2700000000),5200000000);   
+    EXPECT_EQ(func.addition(2500000000,2700000000),5200000000);   
 }
 
 
